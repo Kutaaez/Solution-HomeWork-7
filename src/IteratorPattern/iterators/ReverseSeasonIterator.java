5e51cf639ff19d128a42505fdb97566a40314f1a
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-04-26 15:43:56 +0500

    created reverseSeason i.

---
+++ src/IteratorPattern/iterators/ReverseSeasonIterator.java
@@ -0,0 +1,25 @@
+package IteratorPattern.iterators;
+
+import IteratorPattern.models.Episode;
+import IteratorPattern.models.Season;
+
+import java.util.List;
+
+public class ReverseSeasonIterator implements Iterator{
+    private List<Episode> episodes;
+    private int index ;
+    public ReverseSeasonIterator(Season season) {
+        this.episodes = season.getSeasonEpisodes();
+        this.index = episodes.size() - 1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index >= 0;
+    }
+
+    @Override
+    public Object next() {
+        return episodes.get(index--);
+    }
+}
