47131659186e9e68a6fad1ec19b2595e85cb9912
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-04-26 22:46:36 +0500

    created passengerplane

--- src/MediatorPattern/aircraft/PassengerPlane.java
+++ src/MediatorPattern/aircraft/PassengerPlane.java
@@ -1,4 +1,14 @@
 package MediatorPattern.aircraft;
 
-public class PassengerPlane {
+public class PassengerPlane extends Aircraft {
+
+
+        public PassengerPlane(String id, int fuelLevel) {
+            super(id, fuelLevel);
+        }
+
+        @Override
+        public void receive(String message) {
+            System.out.println("[PassengerPlane " + id + "] received: " + message);
+        }
 }
