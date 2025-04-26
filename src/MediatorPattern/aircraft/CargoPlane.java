5dac60537d76b3a1d5852243ac62804ed8b360d4
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-04-26 22:47:34 +0500

    created cargoplane

--- src/MediatorPattern/aircraft/CargoPlane.java
+++ src/MediatorPattern/aircraft/CargoPlane.java
@@ -1,4 +1,14 @@
 package MediatorPattern.aircraft;
 
-public class CargoPlane {
+
+public class CargoPlane extends Aircraft {
+
+    public CargoPlane(String id, int fuelLevel) {
+        super(id, fuelLevel);
+    }
+
+    @Override
+    public void receive(String message) {
+        System.out.println("[CargoPlane " + id + "] received: " + message);
+    }
 }
