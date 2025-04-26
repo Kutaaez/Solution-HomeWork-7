bc1753f63c747d58da778d6848c3a633368ba33f
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-04-26 22:48:06 +0500

    created helicopter

--- src/MediatorPattern/aircraft/Helicopter.java
+++ src/MediatorPattern/aircraft/Helicopter.java
@@ -1,4 +1,13 @@
 package MediatorPattern.aircraft;
 
-public class Helicopter {
+public class Helicopter extends Aircraft {
+
+    public Helicopter(String id, int fuelLevel) {
+        super(id, fuelLevel);
+    }
+
+    @Override
+    public void receive(String message) {
+        System.out.println("[Helicopter " + id + "] received: " + message);
+    }
 }
