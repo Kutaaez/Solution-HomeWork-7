97fb2df0aa6c89e07053887c47e141ad5f1309f8
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-04-26 15:56:16 +0500

    created shufleseason i.

--- src/IteratorPattern/iterators/ShuffleSeasonIterator.java
+++ src/IteratorPattern/iterators/ShuffleSeasonIterator.java
@@ -1,4 +1,28 @@
 package IteratorPattern.iterators;
 
-public class ShuffleSeasonIterator {
+import IteratorPattern.models.Episode;
+import IteratorPattern.models.Season;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class ShuffleSeasonIterator implements  EpisodeIterator {
+    private List<Episode> shuffledEpisodes;
+    private int position = 0;
+    public ShuffleSeasonIterator(Season season) {
+        this.shuffledEpisodes = new ArrayList<>(season.getSeasonEpisodes());
+        Collections.shuffle(shuffledEpisodes, new Random(45));
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < shuffledEpisodes.size();
+    }
+
+    @Override
+    public Episode next() {
+        return shuffledEpisodes.get(position++);
+    }
 }
