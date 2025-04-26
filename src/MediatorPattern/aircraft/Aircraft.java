2938eaae8398a0844fd6d9a1bb2dadbce96d78fd
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-04-26 20:59:09 +0500

    update aircraft

--- src/MediatorPattern/aircraft/Aircraft.java
+++ src/MediatorPattern/aircraft/Aircraft.java
@@ -4,15 +4,32 @@ import MediatorPattern.mediator.TowerMediator;
 
 public abstract class Aircraft {
     protected String id;
-    protected int fuelLevel;
+    protected int fuelLevel; // fuel level in area of 0 to 100 %
 
     public Aircraft(String id, int fuelLevel) {
         this.id = id;
         this.fuelLevel = fuelLevel;
     }
-
+    //requirment nethod
     public abstract void receive(String msg);
     public void send(String msg, TowerMediator m) {
         m.broadcast(msg, this);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getFuelLevel() {
+        return fuelLevel;
+    }
+    public void decreaseFuel() {
+        fuelLevel = Math.max(fuelLevel - 1, 0);
+    }
+        //getclass().getSimpleName() get class name from object.
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " " + id + " (Fuel: " + fuelLevel + "%)";
     }
 }
